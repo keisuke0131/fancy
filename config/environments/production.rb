3f10f25c1ee0017b368f0d7bacdbfdcb31bbc08e
Keisuke Tomiyoshi <gottue_kanzi0131@yahoo.co.jp>
2020-10-10 01:28:37 +0900

    update css contact

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,6 +103,7 @@ Rails.application.configure do
     enable_starttls_auto: true
     }
   config.action_mailer.perform_caching = false
+  
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
