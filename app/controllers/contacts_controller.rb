f94ba1ac581056dc780112007d0265e4bd9c29b9
Keisuke Tomiyoshi <gottue_kanzi0131@yahoo.co.jp>
2020-10-10 02:34:51 +0900

    update contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -19,6 +19,6 @@ class ContactsController < ApplicationController
   private
 
     def contact_params
-      params.permit(:name,:email, :message)
+      params.require(:contact).permit(:name,:email, :message)
     end
 end
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,10 @@
 class ContactMailer < ApplicationMailer
   def contact_mail(contact)
-    @contact = contact  
-    mail to: "gottue_kanzi0131@yahoo.co.jp", subject: "メールのタイトル"
+      @contact = contact
+      mail(
+        from: 'gobugobu0131@gmail.com',
+        to:   'gottue_kanzi0131@yahoo.co.jp',
+        subject: 'お問い合わせ通知'
+      )
   end
 end
-
