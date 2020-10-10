b02a61ac9014318936d0784d6a5d73260f974aef
Keisuke Tomiyosi <gottue_kanzi0131@yahoo.co.jp>
2020-10-11 01:09:06 +0900

    update css post-show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :admin_check, only:[:new,:create,:edit,:update,:destroy]
 
   def index
-    @posts=Post.all
+    @posts=Post.all.order(id: "DESC")
   end
 
   def show
