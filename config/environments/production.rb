6e336d62bd099eece6676ad1bfc0e5bc51cd1d39
Keisuke Tomiyoshi <gottue_kanzi0131@yahoo.co.jp>
2020-10-15 13:18:53 +0900

    change env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,6 +5,9 @@ Rails.application.configure do
   config.cache_classes = true
   config.force_ssl = true
 
+  host = 'https://www.fancy-jp.work'                     
+  config.action_mailer.default_url_options = { host: host }
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
