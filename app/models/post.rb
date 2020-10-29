46557b749b32adec2bed2508c16222c775c411c0
Keisuke Tomiyosi <gottue_kanzi0131@yahoo.co.jp>
2020-10-29 21:13:52 +0900

    udpate post title

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ApplicationRecord
   has_many :users, through: :likes
   has_one_attached :image
 
+  validates :title, presence: true
+  validates :content, presence: true
+
+
   def liked_by?(user)
     likes.where(user_id: user.id).exists?
   end
