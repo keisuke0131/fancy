5263389999a57440d11d5c8b5319595caa62a811
Keisuke Tomiyosi <gottue_kanzi0131@yahoo.co.jp>
2020-11-03 20:39:14 +0900

    update post column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_16_163533) do
+ActiveRecord::Schema.define(version: 2020_11_03_112117) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2020_10_16_163533) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "content"
+    t.integer "step"
   end
 
   create_table "users", force: :cascade do |t|
